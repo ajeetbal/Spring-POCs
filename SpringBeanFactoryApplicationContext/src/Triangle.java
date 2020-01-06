67988b42dd544af24409293597e0dd8177eab92e
Ajeet Bal <ajeetbal7@gmail.com>
2020-01-06 15:56:52 +0530

    Dependency Injection with Setter and Constructor

--- SpringBeanFactoryApplicationContext/src/Triangle.java
+++ SpringBeanFactoryApplicationContext/src/Triangle.java
@@ -1,16 +1,28 @@
 
 public class Triangle {
 	private String type;
+	private int height;
+
+	// DI with Constructor
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
 
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public int getHeight() {
+		return height;
 	}
 
+//	DI with setter
+//	public void setType(String type) {
+//		this.type = type;
+//	}
+
 	void draw() {
-		System.out.println(getType() + " drawing Triangle!!!!!!!!!!!");
+		System.out.println(getType() + " drawing Triangle with hieght " + getHeight());
 	}
 }
