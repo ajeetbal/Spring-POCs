3e81383e061789bf32c129807abbe86879239521
Ajeet Bal <ajeetbal7@gmail.com>
2020-01-07 12:29:25 +0530

    Initialize Collection

--- SpringInjectObject/src/Triangle.java
+++ SpringInjectObject/src/Triangle.java
@@ -1,8 +1,11 @@
+import java.util.List;
 
 public class Triangle {
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+
+	private List<Point> trianglePoints;
 
 	public Point getPointA() {
 		return pointA;
@@ -28,10 +31,19 @@ public class Triangle {
 		this.pointC = pointC;
 	}
 
+	public List<Point> getTrianglePoints() {
+		return trianglePoints;
+	}
+
+	public void setTrianglePoints(List<Point> trianglePoints) {
+		this.trianglePoints = trianglePoints;
+	}
+
 	void draw() {
 		System.out.println("pointA [" + getPointA().getX() + "," + getPointA().getY() + " ]");
 		System.out.println("pointB [" + getPointB().getX() + "," + getPointB().getY() + " ]");
 		System.out.println("pointC [" + getPointC().getX() + "," + getPointC().getY() + " ]");
-
+		System.out.println("points are " + getTrianglePoints().get(0).getY());
 	}
+
 }
