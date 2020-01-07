603d7074e2bb240d39b2df658d275676b071bf45
Ajeet Bal <ajeetbal7@gmail.com>
2020-01-07 13:14:58 +0530

    Bean Autowiring
    4 types of autowiring:
    constructor,byName,byType,default

---
+++ SpringAutowiring/src/Triangle.java
@@ -0,0 +1,37 @@
+public class Triangle {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	void draw() {
+		System.out.println("pointA [" + getPointA().getX() + "," + getPointA().getY() + " ]");
+		System.out.println("pointB [" + getPointB().getX() + "," + getPointB().getY() + " ]");
+		System.out.println("pointC [" + getPointC().getX() + "," + getPointC().getY() + " ]");
+	}
+
+}
